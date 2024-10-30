a516ccc2894e39789b4ead89e3e75512251c4650
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-30 09:26:14 +0300

    fix backend problem

---
+++ app/api/football/route.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { NextResponse } from 'next/server'
+
+export const POST = async request => {
+  try {
+    const {
+      url,
+      method = 'GET',
+      headers = { 'X-Auth-Token': 'e191e07f2acf423296dd397ab4d29910' },
+      data = {},
+    } = await request.json()
+
+    const response = await axios({
+      url,
+      method,
+      headers,
+      data,
+    })
+
+    return NextResponse.json(response.data)
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.response ? error.response.data : error.message,
+      },
+      { status: error.response ? error.response.status : 500 }
+    )
+  }
+}
--- redux-system/slices/home/matchesSlice.js
+++ redux-system/slices/home/matchesSlice.js
@@ -1,53 +1,45 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-/*
-TTY stands for today , tomorrow and yesterday
-*/
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 export const getMatchesTTY = createAsyncThunk(
-  "/getMatchesTTY",
+  '/getMatchesTTY',
   async (certianDate, ThunkAPI) => {
-    const { rejectWithValue } = ThunkAPI;
+    const { rejectWithValue } = ThunkAPI
 
     try {
-      const res = await axios({
-        method: "GET",
-        url: `${process.env.NEXT_PUBLIC_APICORS}/api.football-data.org/v4/matches?date=${certianDate}`,
-        headers: {
-          "X-Auth-Token": "e191e07f2acf423296dd397ab4d29910",
-        },
-      });
+      const response = await axios.post('/api/football', {
+        url: `https://api.football-data.org/v4/matches?date=${certianDate}`,
+      })
 
-      return res.data;
-    } catch (er) {
-      return rejectWithValue(er);
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error)
     }
   }
-);
+)
 
 const initialState = {
   TTY_Loading: true,
   TTY_Data: null,
   TTY_Error: null,
-};
+}
 
 const matchSlice = createSlice({
-  name: "home_matches",
+  name: 'home_matches',
   initialState,
-  extraReducers: (builder) => {
-    builder.addCase(getMatchesTTY.pending, (state) => {
-      state.TTY_Loading = true;
-    });
+  extraReducers: builder => {
+    builder.addCase(getMatchesTTY.pending, state => {
+      state.TTY_Loading = true
+    })
     builder.addCase(getMatchesTTY.fulfilled, (state, action) => {
-      state.TTY_Loading = false;
-      state.TTY_Data = action.payload.matches;
-    });
+      state.TTY_Loading = false
+      state.TTY_Data = action.payload.matches
+    })
     builder.addCase(getMatchesTTY.rejected, (state, action) => {
-      state.TTY_Loading = false;
-      state.TTY_Error = action.payload.message;
-    });
+      state.TTY_Loading = false
+      state.TTY_Error = action.payload.message
+    })
   },
-});
+})
 
-export const homeMatches = matchSlice.reducer;
+export const homeMatches = matchSlice.reducer
